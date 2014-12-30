9503f696477899d60c6b682e74d3cd1514d0abea
Frank Sun <erpang007@gmail.com>
2014-12-30 15:25:24 -0800

    Added life.js
    Plans are cited…. Well that’s gonna be a long process.

---
+++ life.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chenfs on 12/30/14.
+ * TriQuoWik
+ * Generate random life questions
+ * Progress 1 (DOING) : store questions in a txt file, read the file and return the question.
+ * Progress 2 (TODO)  : store questions in MySQL database, read random index, return the question.
+ * Progress 3 (TODO)  : Progress 2 + grab user's answer, store the question answers to a txt file
+ *            (TODO)  : with question index and user's answer.
+ * Progress 4 (TODO) (PENDING) : store the answer directly to another database, with index, with answers.
+ * Progress 5 (TODO)  : create suggested answer to every possible question, find answers mainly from Quora
+ *            (TODO)  : or wikipedia or google(scholar). process should be static.
+ * Progress 6 (TODO)  : Dynamically search the answer to the question while the user is answering the question.
+ */
