533bb9a8d9115a23886efdb15b6ddfd23717e5c9
dmcoley <dmcoley@uw.edu>
2014-12-29 22:43:09 -0800

    Initial JS commit for Math

---
+++ math.js
@@ -0,0 +1,11 @@
+/* TriQuoWik
+Generates random algebra problems to quiz
+the user on. Will alert if the user answers
+with the proper solution or not */
+
+(function() {
+	"use strict"
+	window.onload = function() {
+		alert("Hello world!");
+	}
+})();
