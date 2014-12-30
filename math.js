7cd68e6b603dad182a44c5cf834daf785b721099
dmcoley <dmcoley@uw.edu>
2014-12-29 23:59:07 -0800

    Math page to interact with the user

--- math.js
+++ math.js
@@ -6,28 +6,31 @@ with the proper solution or not */
 (function() {
 	"use strict"
 	window.onload = function() {
-		$("button").onclick = quiz;	
+		$("generate").onclick = quiz;
+		$("submit").onclick = answer;	
 	}
 
 	// Called when the math button is clicked
 	// Generates a random algebra problem for the user
 	// to try and answer
 	function quiz() {
-		// Random number between 1 - 99999
-		var first = Math.floor(Math.random() * 99999) + 1;
-		var second = Math.floor(Math.random() * 99999) + 1;
-		// Find random operator between addition and subtraction
+		// Random number between 1 - 100
+		var first = Math.floor(Math.random() * 100) + 1;
+		var second = Math.floor(Math.random() * 100) + 1;
+		// Find random operator between +, -, *
 		var operators = [{
 			sign: "+",
 			method: function(a,b){ return a + b; }
 		},{
-			sign: "-"
+			sign: "-",
 			method: function(a,b){ return a - b; }
+		},{
+			sign: "*",
+			method: function(a,b){ return a * b; }
 		}]
 		var selectedOp = Math.floor(Math.random() * operators.length);
-		// Testing code
-		alert(first + " " + operators[selectedOp].sign + " " + second + " = " + 
-			operators[selectedOp].method(first, second));
+		$("problem").innerHTML = first + " " + operators[selectedOp].sign + " " + second;
+		// operators[selectedOp].method(first, second)
 	}
 
 	// Returns elem by ID from the document
